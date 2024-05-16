00ecb5fe4ead2c8060e21faed9805afb89961034
alejandro jorge <alejorpas2@alu.edu.gva.es>
2024-05-16 12:36:01 +0200

    Commit

--- src/main/java/es/ieslavereda/proyectoservidor/controller/PeliculaController.java
+++ src/main/java/es/ieslavereda/proyectoservidor/controller/PeliculaController.java
@@ -1,12 +1,12 @@
 package es.ieslavereda.proyectoservidor.controller;
 
+import es.ieslavereda.proyectoservidor.repository.model.Pelicula;
+import es.ieslavereda.proyectoservidor.repository.model.Usuario;
 import es.ieslavereda.proyectoservidor.service.PeliculaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -24,6 +24,38 @@ public class PeliculaController {
 
         try {
             return new ResponseEntity<>(service.getAllPeliculas(), HttpStatus.OK);
+        }  catch (SQLException e){
+            Map<String,Object> response = new HashMap<>();
+            response.put("code",e.getErrorCode());
+            response.put(("message"),e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/peliculas/{id}")
+    public ResponseEntity<?> getByID(@PathVariable("id") int id){
+        try {
+            Pelicula pelicula = service.getPelicula(id);
+            if (pelicula == null)
+                return new ResponseEntity<>("Pelicula no encontrada", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(pelicula, HttpStatus.OK);
+
+        } catch (SQLException e){
+            Map<String,Object> response = new HashMap<>();
+            response.put("code",e.getErrorCode());
+            response.put(("message"),e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/peliculas/{id}")
+    public ResponseEntity<?> deletePelicula(@PathVariable("id") int id){
+        try {
+            Pelicula pelicula = service.deletePelicula(id);
+            if (pelicula == null)
+                return new ResponseEntity<>("Pelicula no encontrada",HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(pelicula,HttpStatus.OK);
+
         }  catch (SQLException e){
             Map<String,Object> response = new HashMap<>();
             response.put("code",e.getErrorCode());
--- src/main/java/es/ieslavereda/proyectoservidor/service/PeliculaService.java
+++ src/main/java/es/ieslavereda/proyectoservidor/service/PeliculaService.java
@@ -2,7 +2,6 @@ package es.ieslavereda.proyectoservidor.service;
 
 import es.ieslavereda.proyectoservidor.repository.PeliculaRepository;
 import es.ieslavereda.proyectoservidor.repository.model.Pelicula;
-import es.ieslavereda.proyectoservidor.repository.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.sql.SQLException;
@@ -22,7 +21,7 @@ public class PeliculaService {
         return repository.getPelicula(id);
     }
 
-    public Pelicula deleteUsuario(int id) throws SQLException {
+    public Pelicula deletePelicula(int id) throws SQLException {
         return repository.deletePelicula(id);
     }
 
