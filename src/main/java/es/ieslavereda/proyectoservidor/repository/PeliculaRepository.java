ed4d45e148192285cb592047b730e22f217d173d
alejandro jorge <alejorpas2@alu.edu.gva.es>
2024-05-16 12:28:41 +0200

    Commit

--- src/main/java/es/ieslavereda/proyectoservidor/repository/PeliculaRepository.java
+++ src/main/java/es/ieslavereda/proyectoservidor/repository/PeliculaRepository.java
@@ -2,22 +2,15 @@ package es.ieslavereda.proyectoservidor.repository;
 
 import es.ieslavereda.proyectoservidor.repository.model.DataSource;
 import es.ieslavereda.proyectoservidor.repository.model.Pelicula;
+import es.ieslavereda.proyectoservidor.repository.model.Usuario;
 import org.springframework.stereotype.Repository;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class PeliculaRepository implements IPeliculaRepository {
-
-
-    @Override
-    public Pelicula getPelicula(int id) throws SQLException {
-        return null;
-    }
 
     @Override
     public Pelicula addPelicula(Pelicula pelicula) throws SQLException {
@@ -31,13 +24,24 @@ public class PeliculaRepository implements IPeliculaRepository {
 
     @Override
     public Pelicula deletePelicula(int id) throws SQLException {
-        return null;
+        Pelicula pelicula = getPelicula(id);
+        String query = "DELETE FROM cliente WHERE dniCliente = ?"; // Cambiar query
+
+        if (pelicula == null)
+            return null;
+
+        try (Connection connection = DataSource.getMyOracleDataSource().getConnection();
+             PreparedStatement ps = connection.prepareStatement(query)){
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        }
+        return pelicula;
     }
 
     @Override
     public List<Pelicula> getAllPeliculas() throws SQLException {
         List<Pelicula> peliculas = new ArrayList<>();
-        String query = "SELECT idcontenido,TIPO, TITULO, IDIOMA, GENERO, FECHA_ESTRENO, DESCRIPCION, DIRECTOR, ACTORES, DURACION, VALORACION_MEDIA  FROM contenido WHERE EXISTS(SELECT idcontenido from pelicula)";
+        String query = "SELECT idcontenido, TIPO, TITULO, IDIOMA, GENERO, FECHA_ESTRENO, DESCRIPCION, DIRECTOR, ACTORES, DURACION, VALORACION_MEDIA  FROM contenido WHERE EXISTS(SELECT idcontenido from pelicula)";
 
         try (Connection connection = DataSource.getMyOracleDataSource().getConnection();
             Statement statement = connection.createStatement();
@@ -59,6 +63,34 @@ public class PeliculaRepository implements IPeliculaRepository {
             }
         }
         return peliculas;
+    }
+
+    @Override
+    public Pelicula getPelicula(int id) throws SQLException {
+        Pelicula pelicula = null;
+        String query = "SELECT * FROM cliente WHERE dniCliente = ?"; // Cambiar query
+
+        try (Connection connection = DataSource.getMyOracleDataSource().getConnection();
+             PreparedStatement ps = connection.prepareStatement(query)){
+
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next())
+                pelicula = Pelicula.builder().id(rs.getInt(1))
+                        .tipo(rs.getString(2))
+                        .titulo(rs.getString(3))
+                        .idioma(rs.getString(4))
+                        .genero(rs.getString(5))
+                        .fecha_estreno(rs.getDate(6))
+                        .descripcion(rs.getString(7))
+                        .director(rs.getString(8))
+                        .actores(rs.getString(9))
+                        .duracion(rs.getInt(10))
+                        .valoracion_media(rs.getDouble(11))
+                        .build();
+        }
+        return pelicula;
     }
 }
 
--- src/main/java/es/ieslavereda/proyectoservidor/service/PeliculaService.java
+++ src/main/java/es/ieslavereda/proyectoservidor/service/PeliculaService.java
@@ -2,6 +2,7 @@ package es.ieslavereda.proyectoservidor.service;
 
 import es.ieslavereda.proyectoservidor.repository.PeliculaRepository;
 import es.ieslavereda.proyectoservidor.repository.model.Pelicula;
+import es.ieslavereda.proyectoservidor.repository.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.sql.SQLException;
@@ -13,9 +14,16 @@ public class PeliculaService {
     @Autowired
     private PeliculaRepository repository;
 
-
     public List<Pelicula> getAllPeliculas() throws SQLException {
         return repository.getAllPeliculas();
+    }
+
+    public Pelicula getPelicula(int id) throws SQLException {
+        return repository.getPelicula(id);
+    }
+
+    public Pelicula deleteUsuario(int id) throws SQLException {
+        return repository.deletePelicula(id);
     }
 
 }
